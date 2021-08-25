40ff33a51cce3b8932fb8c77326cbbd7db97b03a
shincho0922 <79453715+shincho0922@users.noreply.github.com>
2021-08-25 10:11:01 +0900

    結合テスト実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create, :destroy] do
-    resources :messages, only: [:index, :create]
+    resources :comments, only: :create
   end
 end
